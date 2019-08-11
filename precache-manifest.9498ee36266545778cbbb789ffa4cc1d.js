51be76b8d8cb62c6c38dcc41ac46cc1642d04a78
Andres F <andresavena19@gmail.com>
2019-08-10 21:06:16 -0500

    primer deploy portf react

---
+++ precache-manifest.9498ee36266545778cbbb789ffa4cc1d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ed84e606ec0e9f7cd3afcdff05a5fab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21003d0724ac4479374d",
+    "url": "/static/css/2.797969c2.chunk.css"
+  },
+  {
+    "revision": "0cf05a37a93981bd6684",
+    "url": "/static/css/main.29d671ca.chunk.css"
+  },
+  {
+    "revision": "21003d0724ac4479374d",
+    "url": "/static/js/2.f2b29561.chunk.js"
+  },
+  {
+    "revision": "0cf05a37a93981bd6684",
+    "url": "/static/js/main.ed1e08be.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
